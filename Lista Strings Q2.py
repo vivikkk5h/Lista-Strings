1bb00b55a1e8bd0fccf204944cf10dc43a677619
vivikkk5h <32853820+vivikkk5h@users.noreply.github.com>
2018-03-21 21:16:48 -0300

    Create Lista Strings Q2.py

---
+++ Lista Strings Q2.py
@@ -0,0 +1,10 @@
+str1 = str(input("Digite uma palavra: "))
+str2 = str(input("Digite outra palavra: "))
+if (len(str1)) == (len(str2)):
+  print("Comprimento iguais")
+else: 
+    print("Comprimento diferentes!")
+if str1 == str2: 
+  print("Conteudo iguais!")
+else: 
+  print("Conteudo diferentes!")
